7bde005a9c491ee1bbce0ba5fbda258949d93e7f
Mzhang84 <91894739+Mzhang84@users.noreply.github.com>
2024-01-21 00:34:20 -0500

    Add files via upload

---
+++ Websites/Feedback UI/index.js
@@ -0,0 +1,11 @@
+// refer to the button
+const btnEl = document.getElementById("btn")
+
+
+function sendReview(){
+
+    
+}
+
+//execute getjoke function after clicking the button
+btnEl.addEventListener("click", );
